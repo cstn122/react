e6d92ea0284e2bc19f44486b34b6155f4de97e84
Anne Hsieh <anne_hsieh@pegatroncorp.com>
2023-03-15 16:43:27 +0800

    Move delete function from parent to child

--- 99-exam/src/components/List/List.js
+++ 99-exam/src/components/List/List.js
@@ -1,43 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./List.module.css";
 import Item from "./Item";
+import { FullDataDispatchContext } from "../../FullDataContext/FullDataContext";
 
-const List = ({data, onUpdate, onDelete}) => {
+const List = ({ data, onUpdate }) => {
+  const dispatch = useContext(FullDataDispatchContext);
 
   const updatingHandler = (updating) => {
     console.log('List updating', updating);
     onUpdate(updating);
   };
 
-  const deletingHandler = (deleting) => {
-    console.log('List deleting', deleting);
-    onDelete(deleting);
+  const deleteHandler = (dataToDelete) => {
+    dispatch({
+      type: 'deleted',
+      payload: {
+        id: dataToDelete.id,
+        name: dataToDelete.name,
+        description: dataToDelete.description,
+        is_demo: dataToDelete.is_demo,
+      },
+    })
   };
 
   return (
-    <table className={classes.table}>
-      <tbody>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope="col">Is Demo</th>
-          <th scope="col"></th>
-        </tr>
+    data.length === 0 ? 
+    <p className={classes.text}>No data found.</p> :
+      <table className={classes.table}>
+        <tbody>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Is Demo</th>
+            <th scope="col"></th>
+          </tr>
 
-        {data.map((item) => (
-          <Item
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            description={item.description}
-            is_demo={item.is_demo}
-            onUpdating={updatingHandler}
-            onDeleting={deletingHandler}
-          />
-        ))}
-      </tbody>
-    </table>
+          {data.map((item) => (
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              is_demo={item.is_demo}
+              onUpdating={updatingHandler}
+              onDeleting={deleteHandler}
+            />
+          ))}
+        </tbody>
+      </table>
   );
 };
 
